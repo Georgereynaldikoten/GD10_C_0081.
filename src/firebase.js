0204962c450480c59d15d01e9a2cdf6cca2e3dbd
Georgereynaldikoten <190710081@students.uajy.ac.id>
2021-11-17 15:03:17 +0700

    GD10_C_0081

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+export const db = firebase
+.initializeApp({databaseURL: 'https://guidedvuefire0081-9202b-default-rtdb.firebaseio.com/'})
+.database()
 
 
+export const drinkRef = db.ref('drinks')
 
 
 
--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -109,9 +109,9 @@
 </template>
 
 <script>
-//tambahkan ini untuk import database reference
-
-export default {
+ //tambahkan ini untuk import database reference
+ import { drinkRef } from "../firebase";
+ export default {
   data: () => ({
     dialog: false,
     dialogDelete: false,
@@ -149,6 +149,9 @@ export default {
   }),
 
   //tambahkan attribute firebase disini
+  firebase:{
+    drinks: drinkRef,
+  },
 
   computed: {
     formTitle() {
@@ -179,7 +182,18 @@ export default {
     },
 
     //tambahkan ini untuk delete data
-    deleteItemConfirm() {},
+    deleteItemConfirm() {
+    drinkRef
+    .child(this.editedIndex)
+    .remove()
+    .then(() =>{
+      alert("Berhasil Hapus Data !");
+
+    })
+    .catch((err) => [alert("Gagal Hapus Data: ", err)]);
+    this.closeDelete();
+    },
+    
 
     close() {
       this.dialog = false;
@@ -198,7 +212,25 @@ export default {
     },
 
     // Tambahkan code method save
-    save() {},
+    save() {
+      if (this.editedIndex != -1){
+        drinkRef
+        .child(this.editedIndex)
+        .set(this.editedItem)
+        .then(() =>{
+        alert("Berhasil Edit Data !");
+        })
+        .catch((err) => [alert("Gagal Edit Data: ", err)]);
+      } else {
+        drinkRef
+        .push(this.editedItem)
+        .then(() =>{
+        alert("Berhasil Tambahkan Data !");
+        })
+        .catch((err) => [alert("Gagal TAmbah Data: ", err)]);
+      }
+      this.close();
+    }
   },
 };
 </script>
